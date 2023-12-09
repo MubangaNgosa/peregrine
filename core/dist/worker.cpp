ea8a6d49a1d32c96286fc66ab5e41966e61f97ca
mubangangosa <mubangacbngosa@gmail.com>
2023-12-08 18:51:11 -0800

    each worker can now create a small graph

--- core/dist/worker.cpp
+++ core/dist/worker.cpp
@@ -8,19 +8,17 @@
 volatile sig_atomic_t terminate_flag = 0; // Flag for graceful shutdown
 
 void signal_handler(int /* signal */) {
-    std::cout << "Signal handler triggered." << std::endl;
     terminate_flag = 1;
 }
 
 namespace Peregrine {
 
     SmallGraph createSmallGraphFromJobPacket(const JobPacket& packet) {
-        std::cout << "Creating SmallGraph from JobPacket." << std::endl;
         SmallGraph p;
         for (const auto& edge : packet.edges) {
             p.add_edge(edge.first, edge.second);
         }
-        // Optional: set labels for vertices if needed
+        // Optional: Set labels for vertices
         // for (const auto& vertex : packet.vertices) {
         //     p.set_label(vertex, label_value); // Define label_value as needed
         // }
@@ -35,12 +33,10 @@ int main() {
     std::cout << "Worker starting up..." << std::endl;
 
     // Set up signal handling for graceful shutdown
-    std::cout << "Setting up signal handling." << std::endl;
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
 
     // Initialize ZeroMQ context and sockets
-    std::cout << "Initializing ZeroMQ context and sockets." << std::endl;
     zmq::context_t context(1);
     zmq::socket_t pull_socket(context, ZMQ_PULL);
     pull_socket.connect("tcp://127.0.0.1:5555");
@@ -49,23 +45,21 @@ int main() {
     push_socket.connect("tcp://127.0.0.1:5556");
 
     // Send initial ready message
-    std::cout << "Sending initial ready message." << std::endl;
     zmq::message_t ready_msg("Ready", 5);
-    push_socket.send(ready_msg, zmq::send_flags::none);
+    push_socket.send(ready_msg); // Old API - no flags needed
+    std::cout << "Worker sent initial ready message." << std::endl;
 
     bool shouldTerminate = false;
 
     while (!terminate_flag && !shouldTerminate) {
         zmq::message_t message;
-        std::cout << "Waiting for a message." << std::endl;
-        if (pull_socket.recv(message, zmq::recv_flags::none)) {
+        if (pull_socket.recv(&message)) { // Old API - no flags needed
             std::string received_data(static_cast<char*>(message.data()), message.size());
-            //std::cout << "Worker received message: " << received_data << std::endl;
+            std::cout << "Worker received message: " << received_data << std::endl;
 
             Peregrine::JobPacket jobPacket = Peregrine::JobPacket::deserialize(received_data);
             Peregrine::SmallGraph smallGraph = Peregrine::createSmallGraphFromJobPacket(jobPacket);
 
-            std::cout << "Processing the SmallGraph." << std::endl;
             // Process the SmallGraph here
 
             if (received_data == "terminate") {
@@ -75,18 +69,18 @@ int main() {
             }
 
             if (!terminate_flag) {
-                std::cout << "Sending ready message for next job packet." << std::endl;
                 zmq::message_t ready_msg("Ready", 5);
-                push_socket.send(ready_msg, zmq::send_flags::none);
+                push_socket.send(ready_msg); // Old API - no flags needed
+                std::cout << "Worker sent ready message for next job packet." << std::endl;
             } else {
                 std::cout << "Worker received shutdown signal during processing." << std::endl;
             }
         }
 
         if (shouldTerminate) {
-            std::cout << "Sending termination acknowledgment." << std::endl;
             zmq::message_t ack_msg("Terminated", 10);
-            push_socket.send(ack_msg, zmq::send_flags::none);
+            push_socket.send(ack_msg); // Old API - no flags needed
+            std::cout << "Worker sent termination acknowledgment." << std::endl;
             break;
         }
     }
